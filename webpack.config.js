737158ee9fb1fbf87b926e65bf5d0c7eb8dc866b
Matthew Roseman <mroseman95@gmail.com>
2020-04-15 16:06:56 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: path.join(__dirname, "index.js"),
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        loader: "file-loader"
+      }
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public/"),
+    port: 3000,
+    publicPath: "http://localhost:3000/",
+    hotOnly: true,
+    disableHostCheck: true
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
+};
